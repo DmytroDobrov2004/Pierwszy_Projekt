cd0fef1e7c39ad729e7948594ed57a064e55c4b2
Dobrov Dmytro <dimon.dobrow@gmail.com>
2022-10-06 18:11:47 +0200

    First initiated projekt

---
+++ src/Dla_Projektu/Projekt1.java
@@ -0,0 +1,12 @@
+package Dla_Projektu;
+
+public class Projekt1 {
+    public static void main(String[] args) {
+        int[] a= {12,45,78,37};
+        int result = 0;
+        for(int i=0;i<a.length;i++){
+             result = result +a[i];
+        }
+        System.out.println(result);
+    }
+}
